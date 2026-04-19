79da109188b91acd9c09d2a09d7fbd3d96e35a3b
Mayankkumar47 <mayankkumar25281@gmail.com>
2026-04-19 17:01:56 +0530

    Update frontend BASE_URL for deployment

--- frontend/src/utils/axioInstance.js
+++ frontend/src/utils/axioInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://task-manager-backend-bw2b.onrender.com"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
